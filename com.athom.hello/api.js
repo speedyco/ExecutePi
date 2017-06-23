de462fee8ff7fdba3086d95f0a7a04c0a21e141b
speedyco <rvdijssel@live.nl>
2017-06-23 13:16:25 +0200

    Update api.js

--- com.athom.hello/api.js
+++ com.athom.hello/api.js
@@ -1,9 +1,16 @@
+var defaultOptions = {
+	host: "192.168.2.14",
+	path: ''
+}
+
 module.exports = [
 
     {
         description:		'Get P1 meter data',
         method: 		'GET',
-        path:			'192.168.2.14/',
+        path:			'/',
+        requires_authorization: false,
+        
         fn: function( callback, args ){
             var result = Homey.app.executePiP1();
 
