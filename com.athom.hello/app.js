6cec23892130f3699d91d48ba2262669f3740fd2
speedyco <rvdijssel@live.nl>
2017-06-23 13:27:45 +0200

    Update app.js

--- com.athom.hello/app.js
+++ com.athom.hello/app.js
@@ -2,3 +2,33 @@
 
 module.exports.init = function(){
 	Homey.log("Starting Execute Pi P1");
+	currentP1meterdataHost = Homey.manager('settings').get('currentRaspberryPiHost');
+	currentDataLoation = Homey.manager('settings').get('currentDataLocation');	
+	if(currentP1meterdataHost == null || currentDataLoation == null){
+		Homey.log('No settings selected');
+		Homey.manager('speech-output').say(__('setupneeded'));
+	}
+	
+	Homey.manager('cron').on('action.get_p1meter_data',function(callback,args){
+		Homey.log('Triggered by cron');
+		onScheduleTrigger();
+		callback(null,true);
+	})
+	
+	Homey.log('Done init execute Pi P1 app');
+	
+}
+
+var onScheduleTrigger = function(){
+	Homey.log('Start Schedule trigger');
+	
+	currentP1meterdataHost = Homey.manager('settings').get('currentRaspberryPiHost');
+	currentDataLoation = Homey.manager('settings').get('currentDataLocation');	
+	
+	if(currentCinemaId == null){
+		Homey.manager('speech-output').say(__('setupneeded'));
+		return;
+	}
+}
+
+module.exports.init = init;
