ff7a9334b6f3db90c3107a2e196e202d9675fed0
speedyco <rvdijssel@live.nl>
2017-06-23 12:34:40 +0200

    Create Homey.manager.js

---
+++ com.athom.hello/Homey.manager.js
@@ -0,0 +1,16 @@
+Homey.manager('insights').createLog( 'power_usage', {
+    label: {
+        en: 'Power Usage'
+    },
+    type: 'number',
+    units: {
+        en: 'kWh'
+    },
+    decimals: 2,
+    chart: 'stepLine' // prefered, or default chart type. can be: line, area, stepLine, column, spline, splineArea, scatter
+}, function callback(err , success){
+    if( err ) return console.error(err);
+    Homey.manager('insights').createEntry( 'power_usage', 9, new Date(), function(err, success){
+        if( err ) return console.error(err);
+    })
+});
