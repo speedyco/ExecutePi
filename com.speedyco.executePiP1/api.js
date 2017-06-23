676da6920ef19d96abce07dba695714fc97be611
speedyco <rvdijssel@live.nl>
2017-06-23 14:14:14 +0200

    Clone to new master
    Clone from com.athom.hello

---
+++ com.speedyco.executePiP1/api.js
@@ -0,0 +1,17 @@
+module.exports = [
+
+    {
+        description:		'Get P1 meter data',
+        method: 		'GET',
+        path:			'192.168.2.14/',
+        fn: function( callback, args ){
+            var result = Homey.app.executePiP1();
+
+            // callback follows ( err, result )
+            callback( null, result );
+
+            // access /?foo=bar as args.query.foo
+        }
+    }
+
+]
