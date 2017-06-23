12ddd3e1bd787c29323dc0643236c4b5a3b1a662
speedyco <rvdijssel@live.nl>
2017-06-23 12:31:08 +0200

    Update api.js
    basis api set up

--- com.athom.hello/api.js
+++ com.athom.hello/api.js
@@ -1,1 +1,52 @@
-api
+module.exports = [
+
+    {
+        description:		'Get something',
+        method: 		'GET',
+        path:			'/',
+        fn: function( callback, args ){
+            var result = Homey.app.getSomething();
+
+            // callback follows ( err, result )
+            callback( null, result );
+
+            // access /?foo=bar as args.query.foo
+        }
+    },
+
+    {
+        description:		'Add something new',
+        method: 		'POST',
+        path:			'/',
+        fn: function( callback, args ){
+            var result = Homey.app.addSomething( args.body );
+            callback( null, result );
+        }
+    },
+
+    {
+        description:		'Update something',
+        method: 		'PUT',
+        path:			'/:id',
+        fn: function( callback, args ){
+            var result = Homey.app.updateSomething( args.params.id, args.body );
+            callback( null, result );
+        }
+    },
+
+    {
+        description:		'Delete something',
+        method: 		'DELETE',
+        path:			'/:id',
+        fn: function( callback, args ){
+            var success = Homey.app.deleteSomething( args.params.id );
+
+            if( !result )
+                callback( success, null );
+            } else {
+                callback( null, success );
+            }
+        }
+    }
+
+]
